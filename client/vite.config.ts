c1f7e62667aeba08cdd6428211a5fdd5c759a890
Fetisov <rofetisov@mail.ru>
2023-08-18 14:22:44 +0300

    add server to vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,5 +11,12 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      }
+    }
   }
 })
