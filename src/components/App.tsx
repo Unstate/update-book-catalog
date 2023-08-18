e3dfdeec6c75bf47022529def79972471d3638da
Fetisov <rofetisov@mail.ru>
2023-08-18 14:09:15 +0300

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center bg-slate-500">
+      Hello, world
+    </div>
+  )
+}
+
+export default App
